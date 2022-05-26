64e164895f5192260d551720e45c6c3f0e8c3941
JuanRamirezC <juan.ramirez.castaneda.01@gmail.com>
2022-05-26 23:27:03 +0200

    Third Commit

--- src/Milki/Empresa.java
+++ src/Milki/Empresa.java
@@ -1,4 +1,10 @@
 package Milki;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -8,12 +14,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Empresa {
-
+	
+	//Variables varias
 	private static double BENEFICIO=1.2;
 	private static int numCL=1;
 	private static int numPR=1;
 	private static int codProd=1;
 	private static int numPedido=1;
+	private static File f;
+	private static FileReader fr;
+	private static BufferedReader br;
+	private static FileWriter fw;
+	private static BufferedWriter bw;
 
 	//Conectamos a la base de datos
 	private static String bd="XE";
@@ -41,7 +53,8 @@ public class Empresa {
 		}
 	}
 
-	//personaString dir
+	//METODOS CLIENTE
+
 	public static void anadirCliente(String dni, String nombre, String apellidos, int number, String dir, int postal ) throws SQLException {
 
 		st=connection.createStatement();
@@ -106,6 +119,9 @@ public class Empresa {
 		}
 	}
 
+
+	//METODOS PROVEEDOR
+
 	public static void anadirProv(String dni, String nombre, int number, String dir, int postal) throws SQLException {
 
 		st=connection.createStatement();
@@ -136,23 +152,18 @@ public class Empresa {
 	public static void borrarProv(String codigo) throws SQLException {
 		st= connection.createStatement();
 		rs= st.executeQuery("select * from persona where upper (codigo)='"+codigo+"'");
-
 		if(rs.next()) {
-
 			ps= connection.prepareStatement("delete from persona where upper (codigo)='"+codigo+"'");
 			ps.execute();
 			ps.close();
 			st.close();	
 			System.out.println("Proveedor borrado correctamente");
 		}
-
 		else {
 			System.out.println("Proveedor no encontrado");
 		}
 	}
-
-
-	//producto
+	//METODOS ALMACEN
 
 	public static void verProductos() throws SQLException {
 		int codigo_producto, cant;
@@ -175,6 +186,7 @@ public class Empresa {
 			System.out.println("Codigo de producto: "+codigo_producto+ "	Nombre: "+nombre+ "	Cantidad: "+cant+ "	Descripcion: "+descripcion+ "\nPrecio de Compra: "+precio_compra+ "	Precio de venta: "+precio_venta);
 		}
 	}
+
 
 	public static void anhadirProductoN(String nombre, int cantidad, String descripcion, double preCo) throws SQLException {
 
@@ -210,9 +222,13 @@ public class Empresa {
 			System.out.println("Codigo de producto no existente");
 		}
 	}
-	//pedido_prod... aqui se tiene que almacenar  luego  en la tabla pedidos
-	public static void altaPedido(String persona) throws SQLException { //Pedido
 
+
+
+	//METODOS GENERALES PEDIDOS
+
+	public static void altaPedido(String persona) throws SQLException { //Pedido
+		int pedido=0;
 		st=connection.createStatement();
 
 		rs= st.executeQuery("select max(num_pedido)"+"\"PEDIDO\""+ "from pedido");
@@ -228,45 +244,145 @@ public class Empresa {
 		ps.execute();
 		ps.close();	
 
-		st.close();	
+
 
 		System.out.println("Pedido añadido correctamente");
+
+		rs= st.executeQuery("select max(num_pedido)"+"\"PEDIDO\""+ "from pedido");
+		if(rs.next())
+			pedido=rs.getInt("PEDIDO");
+
+		System.out.println("Numero de pedido: "+pedido);
+		st.close();	
 	}
-	
+
 	public static void cancelarPedido (int numeroped) throws SQLException {
 		st=connection.createStatement();
 		rs= st.executeQuery("select * from pedido where num_pedido="+numeroped);
 		if(rs.next()) {
 			rs= st.executeQuery("update PEDIDO set ESTADO='CANCELADO', FINALIZADO=1 where num_pedido="+numeroped);
-			
-			ps= connection.prepareStatement("delete from pedido_prod where num_pedido="+numeroped);
-			ps.execute();
-			ps.close();	
-			
+			rs= st.executeQuery("delete from pedido_prod where num_pedido="+numeroped);
+
 			System.out.println("Pedido cancelado correctamente");
 		}
 	}
 
-	public static void anhadirProducto_Pedido(int numpedido, int codigoprod, int cant) throws SQLException { //Pedido Prod
+	public static void confirmarPedido(int numpedido) throws SQLException {
+		st=connection.createStatement();
+
+		rs= st.executeQuery("select * from pedido where num_pedido="+numpedido);
+		if(rs.next()) {
+			rs= st.executeQuery("update pedido set estado='CONFIRMADO' where num_pedido="+numpedido);
+		}
+		else {
+			System.out.println("Pedido no existente");
+		}
+	}
+
+	public static void finalizarPedido(int numpedido) throws SQLException { //Calcula trigger trigger importe base
+		int finali;
+		st=connection.createStatement();
+
+		rs= st.executeQuery("select finalizado from pedido where num_pedido="+numpedido);
+		if(rs.next()) {
+			finali=rs.getInt("finalizado");
+			if(finali==0) {			
+				rs= st.executeQuery("update pedido set finalizado=1, estado='FINALIZADO' where num_pedido="+numpedido);
+				System.out.println("Pedido finalizado");
+			}
+			else {
+				System.out.println("Pedido ya se encuentra finalizado");
+			}
+		}
+		else {
+			System.out.println("Pedido no existente");
+		}
+
+	}
+
+	public static void verArticulosPedido(int numeroped) throws SQLException { //revisar error "Nombre de columna no valido"
+		String nombre,aux="";
+		int codigo,  cantidad;
+		double precio, precioTotal;
+
+		st=connection.createStatement();
+		rs= st.executeQuery("select substr(cod_persona,1,2)" +"\"CODIGO\""+"from pedido where num_pedido="+numeroped);
+		if (rs.next()) {
+			aux=rs.getString("CODIGO");
+
+		}
+		if (aux.equals("CL")) {
+			rs= st.executeQuery("select prd.codigo_prod,nombre, cantidad_prod,precio_venta from pedido_prod prd join producto pd on prd.codigo_prod=pd.codigo_prod where num_pedido="+numeroped);
+			System.out.println("-------------------------");
+			System.out.println("PRODUCTOS DEL PEDIDO: " +numeroped);
+			while(rs.next()) {
+				codigo=rs.getInt("codigo_prod");
+				nombre=rs.getString("nombre");
+				cantidad=rs.getInt("cantidad_prod");
+				precio=rs.getDouble("precio_venta");
+
+				precioTotal=cantidad*precio;
+
+				System.out.println("Codigo producto: "+codigo+ "	Nombre del producto: "+nombre+"	Cantidad: "+cantidad+ "	Precio del producto: "+precio+ "	Precio Total: "+String.format("%.2f", precioTotal));
+			}
+			System.out.println("-------------------------");
+		}
+		else {
+			rs= st.executeQuery("select prd.codigo_prod,nombre, cantidad_prod,precio_compra from pedido_prod prd join producto pd on prd.codigo_prod=pd.codigo_prod where num_pedido="+numeroped);
+			System.out.println("-------------------------");
+			System.out.println("PRODUCTOS DEL PEDIDO: " +numeroped);	
+			while(rs.next()) {
+				codigo=rs.getInt("codigo_prod");
+				nombre=rs.getString("nombre");
+				cantidad=rs.getInt("cantidad_prod");
+				precio=rs.getDouble("precio_compra");
+
+				precioTotal=cantidad*precio;
+
+				System.out.println("Codigo producto: "+codigo+ "	Nombre del producto: " +nombre+ "	Cantidad: "+cantidad+ "	Precio del producto: "+precio+ "	Precio Total: "+String.format("%.2f", precioTotal));
+			}
+			System.out.println("-------------------------");
+		}
+	}
+
+
+	// METODOS DENTRO DE PEDIDOS
+
+	public static void anhadirProducto_PedidoCli(int numpedido, int codigoprod, int cant) throws SQLException { //Pedido Prod
 		int cantidad;
+		int codigo=0;
+		int pedido=0;
 		st=connection.createStatement();
 		rs= st.executeQuery("select num_pedido from pedido where num_pedido="+numpedido);
 		if (rs.next()) {
+
+			rs= st.executeQuery("select num_pedido, codigo_prod from pedido_prod where num_pedido="+numpedido+"and codigo_prod="+codigoprod);
+			if (rs.next()) { 
+				pedido=rs.getInt("num_pedido");
+				codigo=rs.getInt("codigo_prod");
+			}
+
 			rs= st.executeQuery("select cantidad from producto where codigo_prod="+codigoprod);
 			if(rs.next()) {
 				cantidad=rs.getInt("cantidad");
-				if(cantidad>=cant) {
-					ps= connection.prepareStatement("insert into pedido_prod values ((?),(?),(?)) ");
-					ps.setInt(1, numpedido);
-					ps.setInt(2, codigoprod);
-					ps.setInt(3, cant);
+				if(cantidad>=cant ) {					
+					if (codigo==codigoprod && pedido==numpedido) {
+						rs= st.executeQuery("update pedido_prod set cantidad_prod=cantidad_prod +"+cant+"where num_pedido="+numpedido+"and codigo_prod="+codigoprod);
+						System.out.println("Producto ya existente, cantidad modificada");
+					}
+					else {
+						ps= connection.prepareStatement("insert into pedido_prod values ((?),(?),(?)) ");
+						ps.setInt(1, numpedido);
+						ps.setInt(2, codigoprod);
+						ps.setInt(3, cant);
 
-					ps.execute();
-					ps.close();	
-					System.out.println("Producto añadido al pedido correctamente");
+						ps.execute();
+						ps.close();	
+						System.out.println("Producto añadido al pedido correctamente");
+					}
 				}
 				else {
-					System.out.println("Error al añadir producto, no existen existencias suficientes");
+					System.out.println("Error al añadir producto, no hay existencias suficientes");
 				}
 			}
 			else {
@@ -275,8 +391,40 @@ public class Empresa {
 		}
 		st.close();
 	}
+	
+	public static void anhadirProducto_PedidoProv(int numpedido, int codigoprod, int cant) throws SQLException { //Pedido Prod
+		int codigo=0;
+		int pedido=0;
+		st=connection.createStatement();
+		rs= st.executeQuery("select num_pedido from pedido where num_pedido="+numpedido);
+		if (rs.next()) {
+			rs= st.executeQuery("select num_pedido, codigo_prod from pedido_prod where num_pedido="+numpedido+"and codigo_prod="+codigoprod);
+			if (rs.next()) { 
+				pedido=rs.getInt("num_pedido");
+				codigo=rs.getInt("codigo_prod");
+					if (codigo==codigoprod && pedido==numpedido) {
+						rs= st.executeQuery("update pedido_prod set cantidad_prod=cantidad_prod +"+cant+"where num_pedido="+numpedido+"and codigo_prod="+codigoprod);
+						System.out.println("Producto ya existente, cantidad modificada");
+						//PROCEDICMINETO IMPORTE
+					}
+					else {
+						ps= connection.prepareStatement("insert into pedido_prod values ((?),(?),(?)) ");
+						ps.setInt(1, numpedido);
+						ps.setInt(2, codigoprod);
+						ps.setInt(3, cant);
 
-
+						ps.execute();
+						ps.close();	
+						System.out.println("Producto añadido al pedido correctamente");
+						//PROCEDICMINETO IMPORTE
+					}
+				}	
+			}
+			else {
+				System.out.println("Codigo de producto no existente");
+			}
+		st.close();
+	}
 
 	public static void modificarCantidadProd_Pedido(int numpedido, int codigoprod, int cant) throws SQLException { //Pedido Prod
 		int cantidad;
@@ -286,16 +434,18 @@ public class Empresa {
 			rs= st.executeQuery("select cantidad from producto where codigo_prod="+codigoprod);
 			if(rs.next()) {
 				cantidad=rs.getInt("cantidad");
-				if(cantidad>cant) {
+				if(cantidad>=cant) {
 					ps= connection.prepareStatement("update pedido_prod set cantidad_prod=(?) where codigo_prod="+codigoprod);
 					ps.setInt(1, cant);
 
 					ps.execute();
 					ps.close();	
 					System.out.println("Pedido modificado correctamente");
+					
+					//PROCEDICMINETO IMPORTE
 				}
 				else {
-					System.out.println("Error al modificar producto, existencias incorrectas");
+					System.out.println("Error al modificar producto, existencias insuficientes");
 				}
 			}
 			else {
@@ -313,96 +463,15 @@ public class Empresa {
 			ps.execute();
 			ps.close();	
 			System.out.println("Pedido modificado correctamente");
+			//PROCEDICMINETO IMPORTE
 		}
 		else {
 			System.out.println("Error al modificar producto, existencias incorrectas");
 		}
 	}
 
-	public static void finalizarPedido(int numpedido) throws SQLException { //Calcula trigger trigger importe base
-		int finali;
-		st=connection.createStatement();
 
-		rs= st.executeQuery("select finalizado from pedido where num_pedido="+numpedido);
-		if(rs.next()) {
-			finali=rs.getInt("finalizado");
-			if(finali==0) {
-				ps= connection.prepareStatement("update pedido set finalizado=(?) where num_pedido="+numpedido);
-				ps.setInt(1, 1);
-				ps.execute();
-				ps.close();	 
-
-				System.out.println("Pedido finalizado");
-
-			}
-
-			else {
-				System.out.println("Pedido ya se encuentra finalizado");
-			}
-		}
-		System.out.println("Codigo de pedido no existente");
-	}
-
-	public static void eliminarPedido(int numpedido) throws SQLException { //Pedidos
-		st=connection.createStatement();
-		rs= st.executeQuery("select num_pedido,codigo_prod from pedido_prod where num_pedido="+numpedido);
-		if (rs.next()) {
-			ps= connection.prepareStatement("delete from pedido_prod where num_pedido="+numpedido);
-			ps.execute();
-			ps.close();	
-			System.out.println("Pedido eliminado correctamente");
-		}
-		else {
-			System.out.println("Error al modificar producto, existencias incorrectas");
-		}
-	}
-
-	public static void verArticulosPedido(int numeroped) throws SQLException { //revisar error "Nombre de columna no valido"
-		String nombre,aux="";
-		int codigo,  cantidad;
-		double precio, precioTotal;
-		
-		st=connection.createStatement();
-		rs= st.executeQuery("select substr(cod_persona,1,2)" +"\"CODIGO\""+"from pedido where num_pedido="+numeroped);
-		if (rs.next()) {
-			aux=rs.getString("CODIGO");
-	
-		}
-		if (aux.equals("CL")) {
-			rs= st.executeQuery("select prd.codigo_prod,nombre, cantidad_prod,precio_venta from pedido_prod prd join producto pd on prd.codigo_prod=pd.codigo_prod where num_pedido="+numeroped);
-			System.out.println("-------------------------");
-			System.out.println("PRODUCTOS DEL PEDIDO: " +numeroped);
-			while(rs.next()) {
-				codigo=rs.getInt("codigo_prod");
-				nombre=rs.getString("nombre");
-				cantidad=rs.getInt("cantidad_prod");
-				precio=rs.getDouble("precio_venta");
-				
-				precioTotal=cantidad*precio;
-				
-				System.out.println("Codigo del producto: "+codigo+ " Nombre del producto: "+nombre+" Cantidad: "+cantidad+ " Precio del producto: "+precio+ " Precio Total: "+String.format("%.2f", precioTotal));
-			}
-			System.out.println("-------------------------");
-		}
-		else {
-			rs= st.executeQuery("select prd.codigo_prod,nombre, cantidad_prod,precio_compra from pedido_prod prd join producto pd on prd.codigo_prod=pd.codigo_prod where num_pedido="+numeroped);
-			System.out.println("-------------------------");
-			System.out.println("PRODUCTOS DEL PEDIDO: " +numeroped);	
-			while(rs.next()) {
-					codigo=rs.getInt("codigo_prod");
-					nombre=rs.getString("nombre");
-					cantidad=rs.getInt("cantidad_prod");
-					precio=rs.getDouble("precio_compra");
-					
-					precioTotal=cantidad*precio;
-					
-					System.out.println("Codigo del producto: "+codigo+ "Nombre del producto: " +nombre+ "Cantidad: "+cantidad+ " Precio del producto: "+precio+ " Precio Total: "+String.format("%.2f", precioTotal));
-				}
-			System.out.println("-------------------------");
-			}
-	}
-
-
+	//METODOS PARA VISUALIZAR PEDIDOS
 
 	public static void verPedidos(String persona) throws SQLException {
 		int importe, codPed;
@@ -418,14 +487,45 @@ public class Empresa {
 			importe=rs.getInt("importe_total");
 
 			System.out.println("Codigo pedido: "+codPed+
-						"\nFecha: "+fechPedido+ " Estado: "+estado+ " Importe "+importe);
-			
-			
+					"\nFecha: "+fechPedido+ " Estado: "+estado+ " Importe "+importe);
 		}
 	}
 
-	public static void generarFactura() {
-		//calcular importe sum cantidad
+	
+	public static void generarFactura(int numpedido) throws SQLException, IOException {
+		String finali;
+		
+		int numPedido;
+		double importeTotal;
+		String cod_persona;
+		Date fecha_pedido;
+		
+		st=connection.createStatement();
+		rs= st.executeQuery("select estado from pedido where num_pedido="+numpedido);
+		if(rs.next()) {
+			finali=rs.getString("estado");	
+			
+			if(finali.equals("FINALIZADO") || finali.equals("CONFIRMADO")) {
+				f= new File (".","Ticket-"+numpedido);
+				fw= new FileWriter (f,true);
+				bw= new BufferedWriter (fw);
+					rs= st.executeQuery("select num_pedido, cod_persona, substr(cod_persona,1,2)"+ "\"INICIALES\""+", nombre, fecha_pedido, importe_total from pedido pd join persona ps on pd.cod_persona=ps.codigo where num_pedido="+numpedido);
+					if(rs.next()) {
+							numPedido=1;
+							importeTotal=1.0;
+							cod_persona="";
+						rs= st.executeQuery("select pp.codigo_prod, nombre, cantidad from pedido_prod pp join producto pr on pp.codigo_prod=pr.codigo_prod where pp.num_pedido="+numpedido);
+						while(rs.next()) {
+							
+						}
+					}
+			}	
+			else {
+				System.out.println("No se puede generar FACTURA, compruebe ESTADO (FINALIZADO O CONFIRMADO)");
+			}
+		}
+		else {
+			System.out.println("Codigo de producto no existente");
+		}
 	}
-
 }
--- src/Milki/Principal.java
+++ src/Milki/Principal.java
@@ -1,43 +1,39 @@
 package Milki;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Principal {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, IOException {
 		Empresa.conectar();
 		
 //		Empresa.anadirCliente("51478798N", "Joel", "Narvaez", 642574885, "Calle Ffdrhtdh", 25022);
-//		
 //		Empresa.borrarCliente(null);
-//		
-//		Empresa.mostrarPedidosCliente(null);
-//		
+//		Empresa.mostrarPedidosCliente(null)
 //		Empresa.anadirProv("A0000003", "JorgeS.A", 661234566, "Merca", 28018);
-//		
 //		Empresa.borrarProv(null);
-//		
 //		Empresa.anhadirProductoN("leche 6L Entera", 10, "Asturiana", 4.00);
-//		Empresa.anhadirProductoN("leche 6L Semi", 20, "Asturiana", 3.80);
-//		
+//		Empresa.anhadirProductoN("leche 6L Semi", 20, "Asturiana", 3.80);	
 //		Empresa.anhadirProductoE( 0, 0);
-		
 //		Empresa.altaPedido("PR2");
 		
-		Empresa.anhadirProducto_Pedido(2, 2, 1);
-		
-//		Empresa.modificarCantidadProd_Pedido(1, 9, 10);
-		
+//		Empresa.anhadirProducto_Pedido(6, 2, 11);
+//		Empresa.anhadirProducto_Pedido(6, 1, 5);
+//		Empresa.anhadirProducto_Pedido(9, 1, 3);
+//		Empresa.anhadirProducto_Pedido(12, 2, 10);
+//		Empresa.modificarCantidadProd_Pedido(9, 1, 2);
+//		Empresa.modificarCantidadProd_Pedido(12, 1, 3);
 //		Empresa.eliminarProducto_Pedido(1,2);
-		
-//		Empresa.eliminarPedido(5);
-		
+//		Empresa.cancelarPedido(12);
 //		Empresa.verPedidos("CL1");
-//		Empresa.verPedidos("PR2");
-		
-//		Empresa.verArticulosPedido(5);
-//		Empresa.verArticulosPedido(1);
-		
-//		Empresa.cancelarPedido(1);
+//		Empresa.verPedidos("PR2");	
+//		Empresa.verArticulosPedido(9);
+//		Empresa.verArticulosPedido(12);
+//		Empresa.verArticulosPedido(1);	
+//		Empresa.cancelarPedido(1);		
+		Empresa.finalizarPedido(2);
+//		Empresa.confirmarPedido(2);
+//		Empresa.generarFactura(6);
 	}
 	
 
